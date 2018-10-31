378a5da287ca55840af7a75d03092c52ca73aa65
berkette <rebecca@malachiarts.com>
2018-10-30 23:24:51 -0700

    paths/main.py

--- paths/main.py
+++ paths/main.py
@@ -19,13 +19,9 @@ def getPageData(path):
     if path in paths:
         opts = paths[path]
         return buildPageData(opts['filename'], opts['content_additions'])
-        #opts = paths[path]
-        #if opts['template']:
-        #    return readTemplate(opts['filename'], opts['content_additions'])
-        #else:
-        #    return readView(opts['filename'])
     else:
         return {'status': 404, 'content': '404 - Page Not Found'}
+
 
 def buildPageData(filename, content_additions={}):
     status = 200
@@ -38,31 +34,3 @@ def buildPageData(filename, content_additions={}):
         content = 'FileNotFoundError: Could not locate the template {}'.format(filepath)
     return {'status': status, 'content': content}
 
-
-def readView(opts):
-    # Views are html files that are not being modified with content
-    # additions
-    status = 200
-    try:
-        open_file = open('views/{}'.format(filename))
-        content = open_file.read()
-    except Exception as err:
-        content = "Error: ", err.message
-        status = 500
-    return {'status': status, 'content': content}
-
-
-
-def readTemplate(filename, content_additions):
-    # Templates get content additions. Maybe these should be python files
-    # rather than htmls? Consider how to insert content into these.
-    # f-strings, maybe? Also want to make sure an error is thrown if the
-    # number of content_additions expected and provided don't match up
-    status = 200
-    try:
-        open_file = open('templates/{}'.format(filename))
-        content = open_file.read().format(content_additions)
-    except Exception as err:
-        content = "Error: ", err.message
-        status = 500
-    return {'status': status, 'content': content}
